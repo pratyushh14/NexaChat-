21d06f7d4feda807b433735c2d8f961f8cf8e4ea
Pratyush Rathore <e22cseu0679@bennett.edu.in>
2025-08-28 22:37:32 +0530

    fix: corrected import casing for ChatList and UserInfo

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./list.css"
-import Userinfo from './userinfo/Userinfo'
-import ChatList from './chatlist/Chatlist'
+import UserInfo from './userinfo/Userinfo'
+import ChatList from './chatlist/ChatList'
+
 const List = () => {
   return (
     <div className='list'>
-      <Userinfo/>
-      <ChatList/>
+      <UserInfo />
+      <ChatList />
     </div>
   )
 }
